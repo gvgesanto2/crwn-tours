f6cf0819cb0152bb0443466bc5e4c8b8a5726c73
Gabriel Santo <gvgesanto2@gmail.com>
2020-08-22 11:19:08 -0300

    setting up the starter project with create-react-app

---
+++ src/views/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="application">
+      <h1 className="heading-lg">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
